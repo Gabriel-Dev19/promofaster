c6bd1ebe49a36862a5b75a84f1a30898c5f95e35
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-18 16:19:34 -0300

    add

--- components/HeaderAndFooter/HeadDefault.jsx
+++ components/HeaderAndFooter/HeadDefault.jsx
@@ -5,7 +5,6 @@ export default function HeadDefault(props) {
   return(
     <Head>
       <title>{(props.title || 'Home') + ' | Promo faster'}</title>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <meta name="description" content="Descrição" />
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
