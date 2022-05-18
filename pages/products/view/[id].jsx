22045e7a4c11b24e5121aad08e54b623856dfa76
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-18 15:20:11 -0300

    add

--- pages/products/view/[id].jsx
+++ pages/products/view/[id].jsx
@@ -1,42 +1,30 @@
 import axios from "axios";
 import LayoutDefault from "../../../layouts/LayoutDefault";
 
-export default function DeletePage ({response}) {
-
-  // function deleteProduct () {
-  //   axios.delete('http://apipromoshop-env.eba-jzpjzwfe.us-east-1.elasticbeanstalk.com/api/products/delete/' + id, product, {
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then((response) => {
-  //     router.push('/')
-  //   }, (response) => {
-  //   })
-  // }
+export default function ViewPage ({ data }) {
 
   return(
-    <LayoutDefault title={response.name} noHeader={true}>
-      <div>About us: {response.name} </div>
-      <div>About us: {response.id} </div>
+    <LayoutDefault title={data.name} noHeader={true}>
+      <div>About us: {data.name} </div>
+      <div>About us: {data.id} </div>
     </LayoutDefault>
   )
 }
 
 export const getStaticProps = async ({ params }) => {
   const { data } = await axios.get(`http://apipromoshop-env.eba-jzpjzwfe.us-east-1.elasticbeanstalk.com/api/product/${params.id}`);
-  const response = data;
   return {
     props: {
-      response,
+      data
     },
   };
 };
 
 export const getStaticPaths = async () => {
   const { data } = await axios.get("http://apipromoshop-env.eba-jzpjzwfe.us-east-1.elasticbeanstalk.com/api/products/");
-  const paths = data.map((product) => ({ params: { id: product.id.toString(), name: product.name } }));
+  const paths = data.map((product) => ({ params: { id: product.id.toString() } }));
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
