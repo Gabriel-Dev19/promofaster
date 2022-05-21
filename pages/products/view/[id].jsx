eba1e2a844cc08450188aa4e8fb8f9e565bd71e0
Gabriel Camurça <gabriel@pyxys.com.br>
2022-05-20 23:21:52 -0300

    add

--- pages/products/view/[id].jsx
+++ pages/products/view/[id].jsx
@@ -1,13 +1,33 @@
 import LayoutDefault from "../../../layouts/LayoutDefault";
 import { useRouter } from "next/router";
-import {connectToDatabase} from '../../../lib/mongodb'
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
-export default function ViewPage ({ products }) {
+export default function ViewPage () {
   const router = useRouter()
+  const [dataBase, setDataBase] = useState([])
+
+  function getProducts() {
+    const dev = process.env.NODE_ENV !== 'production'
+    const DEV_URL = 'http://localhost:3000/'
+    const PROD_URL = 'https://promofaster.vercel.app/'
+    axios.get(`${dev ? DEV_URL : PROD_URL}/api/products`)
+      .then((res) => {
+        setDataBase(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getProducts()
+  })
+
   return(
     <>
       {
-        products.filter((item) => {
+        dataBase.filter((item) => {
           return item.id.toString().includes(router.query.id)
         }).map((item, index) => {
           return(
@@ -22,32 +42,3 @@ export default function ViewPage ({ products }) {
     </>
   )
 }
-
-export async function getStaticProps(ctx) {
-  let { db } = await connectToDatabase();
-  const products = await db
-  .collection('products')
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  .find({})
-  .sort({})
-  .toArray();
-  return {
-      props: {
-          products: JSON.parse(JSON.stringify(products)),
-      },
-  };
-}
-
-export const getStaticPaths = async () => {
-  let { db } = await connectToDatabase();
-  const data = await db
-  .collection('products')
-  .find({})
-  .sort({})
-  .toArray();
-  const paths = data.map((product) => ({ params: { id: product.id.toString() } }));
-  return {
-    paths,
-    fallback: true,
-  };
-};
