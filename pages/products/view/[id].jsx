99d32cf51d861ec334185f24a16681a8d25c14c8
Gabriel Camurça <gabriel@pyxys.com.br>
2022-06-13 21:05:12 -0300

    add

--- pages/products/view/[id].jsx
+++ pages/products/view/[id].jsx
@@ -4,35 +4,35 @@ import axios from 'axios'
 export default function ViewPage ({ response }) {
 
   return(
-    <LayoutDefault title={response.name} noHeader={true}>
-      <div>About us: {response.name} </div>
-      <div>About us: {response.id} </div>
-      <div>About us: {response.description} </div>
+    <LayoutDefault title={'/'} noHeader={true}>
+      <div>About us: {'/'} </div>
+      <div>About us: {'/'} </div>
+      <div>About us: {'/'} </div>
     </LayoutDefault>
   )
 }
 
-export const getStaticProps = async ({ params }) => {
-  let dev = process.env.NODE_ENV !== 'production';
-  const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
-  const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.id}`);
-  const response = data;
-  return {
-    props: {
-      response,
-    },
-  };
-};
-
-export const getStaticPaths = async () => {
-  let dev = process.env.NODE_ENV !== 'production';
-  const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
-  const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get`);
-  const paths = data.map((product) => ({ params: { id: product.id.toString() } }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticProps = async ({ params }) => {
+//   let dev = process.env.NODE_ENV !== 'production';
+//   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
+//   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
+//   const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.id}`);
+//   const response = data;
+//   return {
+//     props: {
+//       response,
+//     },
+//   };
+// };
+// 
+// export const getStaticPaths = async () => {
+//   let dev = process.env.NODE_ENV !== 'production';
+//   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
+//   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
+//   const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get`);
+//   const paths = data.map((product) => ({ params: { id: product.id.toString() } }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
