208ac2fad92a9196f322dfe73fba2044616d8a53
Gabriel-Dev19 <gabriel@pyxys.com.br>
2022-07-24 00:20:43 -0300

    Api secure key

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,7 +1,7 @@
 import { getUniqueProductDB } from '../db/methods'
 
 export default async function handle (req, res) {
-  if (process.env.NEXT_PUBLIC_API_KEY !== 'FCdjakJOq6yRXCEr0L1a9JnFrNmM5UkjDqIM4pTvplFNIODl8vuIXzfEc') {
+  if (req.query.NEXT_PUBLIC_API_KEY !== process.env.NEXT_PUBLIC_API_KEY) {
     console.error('Deixe de ser gaiato kkk você não tem essa permissão!')
   } else {
     const getUniqueProduct = await getUniqueProductDB(Number(req.query.id))
--- pages/api/products/get.js
+++ pages/api/products/get.js
@@ -1,7 +1,7 @@
 import { getProductsDB } from '../db/methods'
 
 export default async function handle (req, res) {
-  if (process.env.NEXT_PUBLIC_API_KEY !== 'FCdjakJOq6yRXCEr0L1a9JnFrNmM5UkjDqIM4pTvplFNIODl8vuIXzfEc') {
+  if (req.query.NEXT_PUBLIC_API_KEY !== process.env.NEXT_PUBLIC_API_KEY) {
     console.error('Você não tem essa permissão!')
   } else {
     const getProducts = await getProductsDB()
