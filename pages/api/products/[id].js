c9457dedd491f018789a452f23105b226eb4ecd7
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-08-15 19:04:44 -0300

    add

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -2,7 +2,7 @@ import { getUniqueProductDB } from '../db/methods'
 
 export default async function handle (req, res) {
   if (req.query.NEXT_PUBLIC_API_KEY_METHOD_GET !== process.env.NEXT_PUBLIC_API_KEY_METHOD_GET) {
-    console.error('Deixe de ser gaiato kkk você não tem essa permissão!')
+    console.error('Você não tem essa permissão!')
   } else {
     const getUniqueProduct = await getUniqueProductDB(Number(req.query.id))
     res.json(getUniqueProduct)
