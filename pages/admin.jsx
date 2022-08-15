ca0c6f628e0e814abc66752c00e251201df83988
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-08-15 18:35:21 -0300

    add

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -648,7 +648,7 @@ export const getServerSideProps = async () => {
   let dev = process.env.NODE_ENV !== 'production';
   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=ObY8DtHs8B0gqU5LE1ci3q4V2vg1ovX7Ld39bFghUjDrHakzYYFTAmxalcdPVKzE5URV9qHYvsNyEUjI6v7KMgoLT0DLSDoYzV4u4tBvA4j11x1nHLhwDSRFu1O5KGSz`);
+  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=FCdjakJOq6yRXCEr0L1a9JnFrNmM5UkjDqIM4pTvplFNIODl8vuIXzfEc`);
   const response = data;
   return {
     props: {
