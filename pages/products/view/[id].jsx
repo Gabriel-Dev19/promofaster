fb466cf5e2072321461d57fb99085ca339b1aca4
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-08-15 18:29:31 -0300

    add

--- pages/products/view/[id].jsx
+++ pages/products/view/[id].jsx
@@ -16,7 +16,7 @@ export const getStaticProps = async ({ params }) => {
   let dev = process.env.NODE_ENV !== 'production';
   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.id}?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.id}?NEXT_PUBLIC_API_KEY_METHOD_GET=ObY8DtHs8B0gqU5LE1ci3q4V2vg1ovX7Ld39bFghUjDrHakzYYFTAmxalcdPVKzE5URV9qHYvsNyEUjI6v7KMgoLT0DLSDoYzV4u4tBvA4j11x1nHLhwDSRFu1O5KGSz`);
   const response = data;
   return {
     props: {
@@ -29,7 +29,7 @@ export const getStaticPaths = async () => {
   let dev = process.env.NODE_ENV !== 'production';
   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=ObY8DtHs8B0gqU5LE1ci3q4V2vg1ovX7Ld39bFghUjDrHakzYYFTAmxalcdPVKzE5URV9qHYvsNyEUjI6v7KMgoLT0DLSDoYzV4u4tBvA4j11x1nHLhwDSRFu1O5KGSz`);
   const paths = data.map((product) => ({ params: { id: product.id.toString() } }));
   return {
     paths,
