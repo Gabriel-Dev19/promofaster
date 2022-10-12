92e7c6e799fb18fcac92fc46113922849f5d4752
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-10-11 23:15:05 -0300

    add

--- pages/products/view/[slug].jsx
+++ pages/products/view/[slug].jsx
@@ -4,36 +4,37 @@ import axios from 'axios'
 export default function ViewPage ({ response }) {
 
   return(
-    <LayoutDefault title={response.name} noHeader={true}>
-      <div>About us: {response.name} </div>
-      <div>About us: {response.id} </div>
-      <div>About us: {response.description} </div>
-      <div>About us: {response.slug} </div>
-    </LayoutDefault>
+    <></>
+    // <LayoutDefault title={response.name} noHeader={true}>
+    //   <div>About us: {response.name} </div>
+    //   <div>About us: {response.id} </div>
+    //   <div>About us: {response.description} </div>
+    //   <div>About us: {response.slug} </div>
+    // </LayoutDefault>
   )
 }
 
-export const getStaticProps = async ({ params }) => {
-  let dev = process.env.NODE_ENV !== 'production';
-  const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
-  const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.slug}?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
-  const response = data;
-  return {
-    props: {
-      response,
-    },
-  };
-};
-
-export const getStaticPaths = async () => {
-  let dev = process.env.NODE_ENV !== 'production';
-  const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
-  const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
-  const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
-  const paths = data.map((product) => ({ params: { slug: String(product.slug) } }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticProps = async ({ params }) => {
+//   let dev = process.env.NODE_ENV !== 'production';
+//   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
+//   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
+//   const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/${params.slug}?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+//   const response = data;
+//   return {
+//     props: {
+//       response,
+//     },
+//   };
+// };
+// 
+// export const getStaticPaths = async () => {
+//   let dev = process.env.NODE_ENV !== 'production';
+//   const DEV_URL = process.env.NEXT_PUBLIC_URL_LOCAL
+//   const PROD_URL = process.env.NEXT_PUBLIC_URL_PROD
+//   const { data } = await axios.get(`${dev ? DEV_URL : PROD_URL}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+//   const paths = data.map((product) => ({ params: { slug: String(product.slug) } }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
