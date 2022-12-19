aefe2d108adb1290e51ea179925e2f4291317780
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-12-19 20:51:02 -0300

    add

--- pages/products/view/[slug].jsx
+++ pages/products/view/[slug].jsx
@@ -13,21 +13,21 @@ export default function ViewPage ({ response = [] }) {
   )
 }
 
-export const getStaticProps = async ({ params }) => {
-  const { data } = await axios.get(`https://promofaster.com.br/api/products/${params.slug}?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
-  const response = data;
-  return {
-    props: {
-      response,
-    },
-  };
-};
-
-export const getStaticPaths = async () => {
-  const { data } = await axios.get(`https://promofaster.com.br/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
-  const paths = data.map((product) => ({ params: { slug: String(product.slug) } }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticProps = async ({ params }) => {
+//   const { data } = await axios.get(`https://promofaster.com.br/api/products/${params.slug}?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+//   const response = data;
+//   return {
+//     props: {
+//       response,
+//     },
+//   };
+// };
+// 
+// export const getStaticPaths = async () => {
+//   const { data } = await axios.get(`https://promofaster.com.br/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`);
+//   const paths = data.map((product) => ({ params: { slug: String(product.slug) } }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
