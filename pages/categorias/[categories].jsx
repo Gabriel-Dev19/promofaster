248856d6c7631af2c60b0576de7d9685f4974465
Gabriel camurça <gabrielcamurcaaa10@gmail.com>
2022-12-23 23:09:30 -0300

    fix

---
+++ pages/categorias/[categories].jsx
@@ -0,0 +1,44 @@
+import LayoutDefault from "../../layouts/LayoutDefault";
+import axios from 'axios'
+import BannerCategory from "../../components/parts/BannerCategory";
+
+export default function CamaMesaEBanho({ infosCategory = [], listProducts = [] }) {
+  return(
+    <LayoutDefault title={infosCategory.title}>
+      <BannerCategory
+        image={infosCategory.bannerInitial.backgroundBanner}
+        configsBg={'no-repeat center center/cover'}
+        configBackdrop={'180deg, rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.75) 60%'}
+        tagTitle={'h1'}
+        slidersPerSwiper={infosCategory.bannerInitial.numberOfSlides}
+        bestOffert={infosCategory.bannerInitial.bestOffert}
+        products={listProducts}
+        filterCategory={infosCategory.bannerInitial.categoriesOfSlides}
+        infos={{
+          title: infosCategory.bannerInitial.titleBanner,
+          description: infosCategory.bannerInitial.descriptionBanner
+        }}
+      />
+      </LayoutDefault>
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const listProducts = (await axios.get(`${process.env.NEXT_PUBLIC_URL_PROD}/api/products/get?NEXT_PUBLIC_API_KEY_METHOD_GET=${process.env.NEXT_PUBLIC_API_KEY_METHOD_GET}`)).data;
+  const infosCategory = (await axios.get(`${process.env.NEXT_PUBLIC_URL_PROD}/api/pages/categories/${params.categories}`)).data;
+  return {
+    props: {
+      listProducts,
+      infosCategory
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL_PROD}/api/pages/categories`);
+  const paths = data.map((categories) => ({ params: { categories: String(categories.slug) } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
